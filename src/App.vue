6e3f8b4a64e2d45ac8fda3e14b7eb6453702a87b
alvaro004 <alvarollano04@gmail.com>
2021-10-31 18:43:39 -0300

    commit antes de hacer deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<section class="form">
+			<form @submit.prevent="">
+				<h1>TASKS APP</h1>
+				<input
+					@keypress.enter="indexBeingEdit === null ? createNewTask() : saveChanges()"
+					v-model="newTask"
+					type="text"
+					placeholder="Write your task"
+				/>
+				<button type="button" @click="indexBeingEdit === null ? createNewTask() : saveChanges()">
+					{{ indexBeingEdit === null ? `Create New Task` : `Save Changes` }}
+				</button>
+			</form>
+		</section>
+		<ul>
+			<li v-for="(task, index) in tasks" :key="index">
+				<span :class="{ 'task-selected': index === indexBeingEdit }"> {{ task }} </span>
+				<div class="buttons">
+					<button
+						class="delete"
+						type="button"
+						:style="{ display: display }"
+						:disabled="isActive"
+						@click="deleteTask(index)"
+					>
+						Delete Task
+					</button>
+					<button class="edit" type="button" @click="editTask(index)">Edit Task</button>
+				</div>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	data() {
+		return {
+			indexBeingEdit: null,
+			newTask: '',
+			tasks: [],
+			isActive: false,
+			display: 'block',
+		};
+	},
+	methods: {
+		createNewTask() {
+			if (this.newTask !== '') {
+				this.tasks.push(this.newTask);
+				this.newTask = '';
+			} else {
+				alert('You need to write a Task!');
+			}
+		},
+		deleteTask(index) {
+			this.tasks = this.tasks.filter((_, i) => index !== i);
+		},
+		editTask(index) {
+			this.newTask = this.tasks[index];
+			this.indexBeingEdit = index;
+			this.isActive = true;
+			this.display = 'none';
+		},
+		saveChanges() {
+			this.tasks[this.indexBeingEdit] = this.newTask;
+			this.newTask = '';
+			this.indexBeingEdit = null;
+			this.isActive = false;
+			this.display = 'block';
+		},
+	},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
